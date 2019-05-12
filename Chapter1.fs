9747cff40a8869e17ec7610668798c6d305283c7
JohannesJoestar <thejohannesjoestar@gmail.com>
2019-05-12 14:00:57 +0200

    signature changes
    Forced types removed, handing it to type inference.

--- Chapter1.fs
+++ Chapter1.fs
@@ -5,11 +5,11 @@ open System
 let g n = (n + 4);
 
 // 1.2
-let h (x: float, y: float) = Math.Sqrt((x * x) + (y * y));;
+let h x y = Math.Sqrt((x * x) + (y * y));;
 
 // 1.3
 let g' = fun n -> (n + 4);;
-let h' = fun (x: float, y: float) -> Math.Sqrt((x * x) + (y * y));;
+let h' = fun x y -> Math.Sqrt((x * x) + (y * y));;
 
 // 1.4
 let rec f = function
