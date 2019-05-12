637173837140f111ec41924c931994adc5dab48a
JohannesJoestar <thejohannesjoestar@gmail.com>
2019-05-13 00:30:32 +0200

    4.2 and 4.3 implemented

--- Chapter4.fs
+++ Chapter4.fs
@@ -3,6 +3,17 @@ let rec upto = function
 | 0 -> []
 | n -> (upto (n - 1)) @ [n]
 
+// 4.2
+let rec downto1 = function
+| 0 -> []
+| n -> n :: (downto1 (n - 1))
+
+// 4.3
+let rec evenN = function
+| 0 -> [0]
+| n when (n % 2 = 0) -> (evenN (n - 1)) @ [n]
+| n -> (evenN (n - 1)) 
+
 // 4.10
 let prefix (a: int list) (b: int list) = 
     if (a.Length > b.Length) then false else
