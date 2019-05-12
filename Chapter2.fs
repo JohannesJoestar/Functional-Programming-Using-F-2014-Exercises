8d2f473f904543ae24864fbf02fb35e3920107b6
JohannesJoestar <thejohannesjoestar@gmail.com>
2019-05-12 14:07:39 +0200

    formatting and type inference

--- Chapter2.fs
+++ Chapter2.fs
@@ -1,7 +1,5 @@
-open System;;
-
 // 2.1
-let f (n: int) = ((n % 2 = 0) && (n % 3 = 0)) && (n % 5 <> 0);;
+let f n = ((n % 2 = 0) && (n % 3 = 0)) && (n % 5 <> 0);;
 
 // 2.2
 let rec pow = function
@@ -12,11 +10,11 @@ let rec pow = function
 let isIthChar (str: string, i, ch) = str.[i] = ch;;
 
 // 2.4
-let rec occFromIth (str: string, i: int, ch: char) = 
+let rec occFromIth (str: string, i, ch) = 
     if (i >= str.Length) then 0 else (if (str.[i] = ch) then 1 else 0) + occFromIth (str, (i + 1), ch);
 
 // 2.5
-let occInString (str: string, ch: char) = occFromIth (str, 0, ch);;
+let occInString (str, ch) = occFromIth (str, 0, ch);;
 
 // 2.6
 let notDivisible (d, n) = (n % d <> 0);;
