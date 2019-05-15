2c4ee3624e04f662a96836fd7c450156246422ef
JohannesJoestar <thejohannesjoestar@gmail.com>
2019-05-15 18:32:38 +0200

    5.2 implemented

--- Chapter5.fs
+++ Chapter5.fs
@@ -1,2 +1,6 @@
 // 5.1
 let filter f l = List.foldBack (fun e a -> if (f e) then (e :: a) else a) l []
+
+// 5.2
+let revrev list = 
+    List.fold (fun acc v -> [(List.fold (fun a e -> e :: a) [] v)] @ acc) [] list
