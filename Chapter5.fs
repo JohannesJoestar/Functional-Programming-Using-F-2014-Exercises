586df123136df88c58b49a7d906e8f7d4da33e20
JohannesJoestar <thejohannesjoestar@gmail.com>
2019-05-15 18:37:43 +0200

    5.3 implemented

--- Chapter5.fs
+++ Chapter5.fs
@@ -4,3 +4,8 @@ let filter f l = List.foldBack (fun e a -> if (f e) then (e :: a) else a) l []
 // 5.2
 let revrev list = 
     List.fold (fun acc v -> [(List.fold (fun a e -> e :: a) [] v)] @ acc) [] list
+
+// 5.3
+let sum p list = List.fold (fun a v -> (match (p v) with
+                                        | true -> v :: a
+                                        | false -> a)) [] list
